aaa6bb6d0065487352977b6a0a905b168d015412
rajperficient01 <rajl.perficient@gmail.com>
2022-06-08 13:20:58 +0530

    added Install button for PWA install popup

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
+
+
+
 const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
 
 /* Put code here */
+
+//1
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  console.log('👍', 'beforeinstallprompt', event);
+  window.deferredPrompt = event;
+  divInstall.classList.toggle('hidden', false);
+});
+
+//2
 butInstall.addEventListener('click', async () => {
   console.log('👍', 'butInstall-clicked');
   const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
-    // The deferred prompt isn't available.
+    
     return;
   }
-  // Show the install prompt.
+  
   promptEvent.prompt();
-  // Log the result
+  
   const result = await promptEvent.userChoice;
   console.log('👍', 'userChoice', result);
-  // Reset the deferred prompt variable, since
-  // prompt() can only be called once.
+ 
   window.deferredPrompt = null;
-  // Hide the install button.
+ 
   divInstall.classList.toggle('hidden', true);
 });
 
-
-//Track the install event////
+//3
 window.addEventListener('appinstalled', (event) => {
   console.log('👍', 'appinstalled', event);
-  // Clear the deferredPrompt so it can be garbage collected
   window.deferredPrompt = null;
 });
-
-
-
-
-
-
 
 
 
